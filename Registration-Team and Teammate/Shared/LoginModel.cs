8838477a4145b02c66d117144c1deb4f450bb4ba
Александр Краснов <krsasha200393@gmail.com>
2022-03-07 11:00:01 +0300

    Add MongoDB, and realize btnrandom

--- Registration-Team and Teammate/Shared/LoginModel.cs
+++ Registration-Team and Teammate/Shared/LoginModel.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Registration_Team_and_Teammate.Shared
 {
     public class LoginModel
     {
+        public LoginModel(string name, string surname, string email, string login)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Login = login;
+        }
+
+        [BsonIgnoreIfDefault]
+        [BsonId]
+        public ObjectId id { get; set; }
+        [BsonElement]
         public string Name { get; set; }
+        [BsonElement]
         public string Surname { get; set; }
+        [BsonElement]
         public string Email { get; set; }
-        public string Password { get; set; }
-        public bool RememberMe { get; set; } 
+        [BsonElement]
+        public string Login { get; set; }
+        public void Add(LoginModel login)
+        {
+            MongoClient client = new MongoClient(); // чтобы подключится к серверу надо передать в качестве аргумента {uri}
+            var db = client.GetDatabase("Users");
+            var collection = db.GetCollection<LoginModel>("User");
+            collection.InsertOne(login);
+        }
+
+        public static List<LoginModel> TakeList()
+        {
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("Users");
+            var collection = db.GetCollection<LoginModel>("User");
+            List<LoginModel> lst = collection.AsQueryable().ToList();
+            return lst;
+        }
     }
 }
+
